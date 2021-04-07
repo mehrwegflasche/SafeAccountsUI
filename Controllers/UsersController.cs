5691c394e0e276ad8c4204456ed8c14c86b01792
nickpavini <nickpavini@gmail.com>
2021-04-07 07:22:05 -0700

    very basic playing around while I figure out exactly how I want the backend c# to communicate with the seperately hosted API

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SafeAccountsUI.Models;
+
+namespace SafeAccountsUI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("login")]
+        public string User_Login([FromBody]string credentials)
+        {
+            return "In Login section of Controller.. ";
+        }
+
+        [HttpPost("sign-up")]
+        public string User_SignUp([FromBody]string userInfo)
+        {
+            return "In Login section of Controller.. ";
+        }
+    }
+}
